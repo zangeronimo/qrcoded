6aa166d53cd82a611507e443716e6f09fd7a431a
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-04-29 16:36:49 -0300

    in development

--- qrcoded.go
+++ qrcoded.go
@@ -18,14 +18,23 @@ func main() {
 	}
 	defer file.Close()
 
-	err = GenerateQRCode(file, "555-2368")
+	err = GenerateQRCode(file, "555-2368", Version(1))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 //GenerateQRCode receive a string and return a slice of strings
-func GenerateQRCode(w io.Writer, code string) error {
-	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+func GenerateQRCode(w io.Writer, code string, version Version) error {
+	size := version.PatternSize()
+	img := image.NewNRGBA(image.Rect(0, 0, size, size))
 	return png.Encode(w, img)
 }
+
+//PatternSize Derive the size from version
+func (v Version) PatternSize() int {
+	return 4*int(v) + 17
+}
+
+//Version version control for the GenerateQRCode
+type Version int8
